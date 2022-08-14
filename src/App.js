33c5549d5b6a83fdd9690d586dc5ee2a38073804
AkshaySharma-BharatPe <akshay.sharma@bharatpe.com>
2022-08-14 15:14:04 +0530

    resolved error

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import img1 from './assets/1.jpeg';
 import img2 from './assets/2.jpg';
 import img3 from './assets/3.jpeg';
 import img4 from './assets/4.png';
-import img5 from './assets/5.jpeg';
 
 import './App.css';
 
