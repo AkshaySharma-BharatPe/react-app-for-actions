0661ef29405ff60d642445eea23ee5b491d07c34
AkshaySharma-BharatPe <akshay.sharma@bharatpe.com>
2022-08-24 16:43:27 +0530

    removed assets

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import logo from './logo.svg';
-import logo1 from './assets/1.jpeg';
-import logo2 from './assets/2.jpeg';
 
 import './App.css';
 
@@ -9,8 +7,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <img src={logo1} className="App-logo" alt="logo" />
-        <img src={logo2} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
